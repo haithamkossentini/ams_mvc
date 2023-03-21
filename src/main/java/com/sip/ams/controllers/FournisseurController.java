7acf6939f98b1c6714819156faa9ea9f56fe7367
haitham kossentini <haitham.kossentini@esprit.tn>
2023-03-21 01:12:23 +0100

    Listing FR en marche

--- src/main/java/com/sip/ams/controllers/FournisseurController.java
+++ src/main/java/com/sip/ams/controllers/FournisseurController.java
@@ -2,17 +2,21 @@ package com.sip.ams.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sip.ams.entities.Fournisseur;
 import com.sip.ams.repositories.FournisseurRepository;
 import com.sip.ams.services.FournisseurService;
 @Controller
-@RequestMapping("/fournisseur")
+@RequestMapping("/fournisseurs")
 public class FournisseurController {
 
 	@Autowired
@@ -36,5 +40,23 @@ public class FournisseurController {
         
         return "fournisseur/listFournisseurs";
 
+    }
+    @GetMapping("add")
+    public String showAddFournisseurForm(Model model) {
+    	Fournisseur fournisseur = new Fournisseur();// object dont la valeur des attributs par defaut
+    	model.addAttribute("fournisseur", fournisseur);
+        return "fournisseur/addFournisseur";
+    }
+    
+    @PostMapping("add")
+    public String addProvider(@Valid Fournisseur fournisseur, BindingResult result) {
+        if (result.hasErrors()) {
+            return "fournisseur/addFournisseur";
+        }
+        if(fournisseur.getNom()=="")
+        	fournisseur.setNom(null);
+        //providerRepository.save(provider);
+        this.fournisseurService.persistFournisseur(fournisseur);
+        return "redirect:list";
     }
 }
